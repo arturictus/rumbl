95d658064ee72e5756a27968c554400bcc098f34
Artur Pañach <arturictus@gmail.com>
2016-06-23 00:18:55 +0200

    Using Channels

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -4,8 +4,11 @@
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
 import {Socket} from "phoenix"
-
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+let socket = new Socket("/socket", {
+  params: {token: window.userToken},
+  logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data) }
+})
+export default socket
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
